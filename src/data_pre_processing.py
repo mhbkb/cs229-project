dd85962f504dbd887d7dbd42fa06a9391c7750e8
Hao Mao <hmao@linkedin.com>
2019-11-07 17:43:08 -0800

    Complete data processing.
    1. Call load_and_clean_data() will return the preprocessed data;
    2. It will store the preprocessed data to a new file so that it won't get lost if in the middle of other processing;
    3. Parameterize the preprocessing options so that we can do different preprocessing combinations;
    4. Uploaded the 88MB processed data with all options ON.

--- src/data_pre_processing.py
+++ src/data_pre_processing.py
@@ -1,73 +1,109 @@
 # System import
 import os
+import time
 import string
 from collections import defaultdict
 
 # External import
 import pandas as pd
 from nltk.corpus import stopwords
+from nltk.stem.snowball import SnowballStemmer
 from nltk.tokenize import RegexpTokenizer
 from tqdm import tqdm
 
 
-tqdm.pandas
+tqdm.pandas()
 
-TRAIN_PATH = '../train.csv'
+TRAIN_PATH = '../train_shuffle1.csv'
 
 
 def load_data(file_path):
-	return pd.read_csv(os.path.join(os.path.dirname(os.path.abspath(__file__))
-, file_path))
+	return pd.read_csv(os.path.join(os.path.dirname(os.path.abspath(__file__)), file_path))
 
-data = load_data(TRAIN_PATH)
-print(data.shape)
+# data = load_data(TRAIN_PATH)
+# print(data.shape)
 
 
 def remove_punctuations(text):
-    return ''.join([t for t in text if t not in string.punctuation])
+	return ''.join([t for t in text if t not in string.punctuation])
 
 # print(remove_punctuations('ab#//#adfaacd# adf #/a'))
 
 
 def init_tokenizer():
-    return RegexpTokenizer(r'\w+')
+	return RegexpTokenizer(r'\w+')
 
 
-def remove_stopwords(text):
-    """
-        Make sure to run:
-            ```
-            import nltk
-            nltk.download()
-            ```
-        in the virtual venv.
-    """
-    return [t for t in text if t not in stopwords.words('english')]
+def remove_stopwords(words):
+	"""
+		Make sure to run:
+			```
+			import nltk
+			nltk.download()
+			```
+		in the virtual venv.
+	"""
+	return ' '.join([word for word in words if word not in stopwords.words('english')])
 
-# print(remove_stopwords(['I', 'see', 'you', 'a', 'monster']))
-
-def clean_data(pd_data):
-    pd_data['question_text'] = pd_data['question_text'].apply(lambda x: remove_punctuations(x))
-    pd_data['question_text'] = pd_data['question_text'].apply(lambda x: init_tokenizer().tokenize(x.lower()))
-    pd_data['question_text'] = pd_data['question_text'].apply(lambda x: remove_stopwords(x))
-
-    print(pd_data['question_text'].head(15))
+# print(remove_stopwords('I see you a monster'))
 
 
-def load_and_clean_data(data_path=TRAIN_PATH):
-    pd_data = load_data(data_path)
-    clean_data(pd_data)
-    return pd_data
+def init_stemmer():
+	return SnowballStemmer('english')
+
+# print(init_stemmer().stem('I am fishing'))
 
 
-def build_word_dict(pd_data):
-    word_dict = defaultdict(int)
-    pd_data = load_and_clean_data()
-    
-    for word in tqdm(pd_data['question_text'].values):
-        word_dict[word] += 1
-        
-    return word_dict
+def clean_data(pd_data, remove_punctuations=True, tokenize=True, remove_stopwords=True, stemming=True):
+	if remove_punctuations:
+		print('Running remove_punctuations...')
+		pd_data['question_text'] = pd_data['question_text'].apply(lambda x: remove_punctuations(x))
+		print(pd_data['question_text'].head(15))
+
+	if tokenize:
+		print('Running tokenize...')
+		pd_data['question_text'] = pd_data['question_text'].apply(lambda x: init_tokenizer().tokenize(x.lower()))
+		print(pd_data['question_text'].head(15))
+
+	if remove_stopwords:
+		# Removing stopwords takes about 30 mins in Hao's machine.
+		print('Removing stopwords...')
+		pd_data['question_text'] = pd_data['question_text'].apply(lambda x: remove_stopwords(x))
+		print(pd_data['question_text'].head(15))
+
+	if stemming:
+		print('Stemming...')
+		pd_data['question_text'] = pd_data['question_text'].apply(lambda x: init_stemmer().stem(x))
+		print(pd_data['question_text'].head(15))
+
+	print(pd_data['question_text'].head(15))
 
 
-# pd_data = load_and_clean_data()
+def load_and_clean_data(data_path=TRAIN_PATH, 
+						output_file='preprocess_{}.csv', 
+						remove_punctuations=True, 
+						tokenize=True,
+						remove_stopwords=True,
+						stemming=True):
+	pd_data = load_data(data_path)
+	clean_data(pd_data, remove_punctuations, tokenize, remove_stopwords, stemming)
+
+	# Since it may take some time to preprocess data, so let's save the pd_data to a file.
+	if output_file:
+		pd_data.to_csv(os.path.join(os.path.dirname(os.path.abspath(__file__)), 
+									output_file.format(time.strftime("%Y%m%d-%H%M%S"))), 
+					   index=False)
+
+	return pd_data
+
+
+# def build_word_dict(pd_data):
+# 	word_dict = defaultdict(int)
+# 	pd_data = load_and_clean_data()
+	
+# 	for word in tqdm(pd_data['question_text'].values, disable=False):
+# 		word_dict[word] += 1
+		
+# 	return word_dict
+
+pd_data = load_and_clean_data()
