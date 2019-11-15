26d5ced5e9353bf1672e5ee35fed04b8c2ff4ced
mhbkb <haomao@haodeMacBook-Pro.local>
2019-11-15 09:04:10 -0800

    Test test.csv with LR.

--- src/logistic_regression.py
+++ src/logistic_regression.py
@@ -10,7 +10,6 @@ from sklearn.model_selection import train_test_split
 # Internal import
 from data_pre_processing import load_data
 
-
 # dev score is: 0.9506938463010814
 # TRAIN_PATH = 'preprocess_without_punctuation.csv'  
 
@@ -32,23 +31,41 @@ TRAIN_PATH = 'preprocess_tokenize_and_stem.csv'
 # dev score is: 0.9504986123073978
 # TRAIN_PATH = 'preprocess_all.csv' 
 
+TEST_PATH = 'test.csv'
 
-def prepare_data():
+
+def prepare_data(load_test_data=False):
 	pd_data = load_data(TRAIN_PATH)
+	vector = TfidfVectorizer("english")
 
-	feature_matrics = TfidfVectorizer("english").fit_transform(pd_data['question_text'].values.astype('U'))
+	feature_matrics = vector.fit_transform(pd_data['question_text'].values.astype('U'))
 
 	# shuffle=False means pick the last 20% as dev data set.
-	return train_test_split(feature_matrics, pd_data['target'], test_size=0.2, shuffle=False)
+	if load_test_data:
+		test_data = load_data(TEST_PATH)
+		test_feature_matrics = vector.transform(test_data['question_text'].values.astype('U'))
+		return feature_matrics, test_feature_matrics, pd_data['target'], test_data
+	else:
+		return train_test_split(feature_matrics, pd_data['target'], test_size=0.2, shuffle=False)
 
 
-def fit_and_predict(train_data, test_data, train_label, test_label):
+def fit_and_predict(load_test_data, train_data, test_feature_matrics, train_label, test_label_OR_test_data):
 	model = LogisticRegression(solver='liblinear', penalty='l2')
 	model.fit(train_data, train_label)
-	prediction = model.predict(test_data)
-	return accuracy_score(test_label, prediction)
+	prediction = model.predict(test_feature_matrics)
 
+	if load_test_data:
+		del test_label_OR_test_data['question_text']
+		# import pdb; pdb.set_trace()
+		test_label_OR_test_data.insert(1, 'prediction', prediction)
+		test_label_OR_test_data.to_csv('submission.csv', index=False)
+		return prediction
+	else:
+		return accuracy_score(test_label_OR_test_data, prediction)
 
 if __name__ == "__main__":
-	score = fit_and_predict(*prepare_data())
-	print(f'dev score is: {score}')
+	load_test_data = True
+    
+	train_data, test_data, train_label, test_label = prepare_data(load_test_data)
+	score = fit_and_predict(load_test_data, train_data, test_data, train_label, test_label)
+	# print(f'dev score is: {score}')
