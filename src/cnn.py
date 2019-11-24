b55343f9306886d96f6a05b6fed8a8fbbb3f02b0
Hao Mao <hao.mao@coinbase.com>
2019-11-24 11:26:03 -0800

    CNN finished 80% of the dev.

---
+++ src/cnn.py
@@ -0,0 +1,66 @@
+# System import
+
+# External import
+import torch
+import psutil
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, confusion_matrix, f1_score, roc_auc_score, roc_curve
+from sklearn.model_selection import train_test_split
+
+
+# Internal import
+from data_pre_processing import build_word_dict, load_data
+from utils import timer
+from cnn_models import *
+
+TRAIN_PATH = 'preprocess.csv'
+TEST_PATH = 'test.csv'
+
+print(psutil.cpu_count())
+
+def get_data():
+	return load_data(TRAIN_PATH)
+
+
+def prepare_data():
+	word_dict = build_word_dict()
+	embeddings = EmbeddingLayer(len(word_dict))
+
+	return embeddings
+
+
+@timer
+def fit_and_predict(load_test_data, 
+					train_data, 
+					test_feature_matrics, 
+					train_label, 
+					test_label_OR_test_data,
+					if_plt_roc):
+	
+
+	model.fit(train_data, train_label)
+
+	prediction = model.predict(test_feature_matrics)
+
+	if load_test_data:
+		del test_label_OR_test_data['question_text']
+		
+		test_label_OR_test_data.insert(1, 'prediction', prediction)
+		test_label_OR_test_data.to_csv('submission.csv', index=False)
+		return prediction
+	else:
+		if if_plt_roc:
+			plt_roc(test_label_OR_test_data, prediction)
+
+		print(f'accuracy is: {accuracy_score(test_label_OR_test_data, prediction)}')
+		print(f'f1 score is: {f1_score(test_label_OR_test_data, prediction)}')
+		print(f'confusion_matrix score is: {confusion_matrix(test_label_OR_test_data, prediction)}')
+
+
+if __name__ == "__main__":
+	load_test_data = False
+    
+	embeddings = prepare_data()
+	train_test_split(embeddings, pd_data['target'], test_size=0.2, shuffle=False)
+	fit_and_predict(load_test_data, train_data, test_data, train_label, test_label, if_plt_roc=True)
--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,48 @@
+import time
+
 import matplotlib.pyplot as plt
+import numpy as np
 from sklearn.metrics import roc_auc_score, roc_curve
+from tqdm import tqdm
 
 def plt_roc(label, prediction):
 	tp, fp, _ = roc_curve(label, prediction)
 	auc = roc_auc_score(label, prediction)
 	plt.plot(tp, fp, label=f'Auc is {auc}')
 	plt.show()
+
+
+def timer(fn_to_measture):
+	def func(*args):
+		start = time.time()
+		res = fn_to_measture(*args)
+		time_cost = time.time() - start
+		print(f'Spent {time_cost} s to run {fn_to_measture}')
+
+		return res
+
+	return func
+    
+
+def load_embeddings(file_path, word_count, num_words):
+	print(f'load_embeddings: {file_path}')
+	embeddings_index = {}
+
+	with open(file_path, encoding='utf-8') as file:
+		for line in file:
+			values = line.split(' ')
+			embeddings_index[values[0]] = np.asarray(values[1:], dtype='float32')
+
+	all_embs = np.stack(embeddings_index.values())
+    embedding_mean, embedding_std = all_embs.mean(), all_embs.std()
+    embedding_size = all_embs.shape[1]
+
+    embedding_matrix = np.random.normal(embedding_mean, embedding_std, (num_words + 1, embedding_size))
+
+	for idx, word in tqdm(enumerate(word_count)):
+		embedding_vect = embeddings_index[word]
+
+		if embedding_vect is not None:
+			embedding_matrix[idx] = embedding_vect
+
+	return embedding_matrix
